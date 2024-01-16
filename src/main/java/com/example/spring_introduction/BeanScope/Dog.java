9c1c86fe38970041acd20035a26428fdf0d6eac8
Rasim Zakirov <rasimzakirov@MacBook-Pro.local>
2024-01-16 22:35:42 +0400

    scope is finished

--- src/main/java/com/example/spring_introduction/BeanScope/Dog.java
+++ src/main/java/com/example/spring_introduction/BeanScope/Dog.java
@@ -5,6 +5,15 @@ import com.example.spring_introduction.Bean.Pet;
 import static java.lang.System.out;
 
 class Dog implements Pet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
 
     public Dog() {
         out.println("Dog bean is created");
--- src/main/java/com/example/spring_introduction/BeanScope/Main.java
+++ src/main/java/com/example/spring_introduction/BeanScope/Main.java
@@ -11,22 +11,48 @@ public class Main {
 
         try {
             Dog myDog = context.getBean("myPet", Dog.class);
-            Dog yourDog = context.getBean("myPet", Dog.class);
+            myDog.setName("Belka");
 
-            out.println(myDog == yourDog);
+            Dog yourDog = context.getBean("myPet", Dog.class);
+            yourDog.setName("Strelka");
+
+
+            out.println(myDog.getName());
+            out.println(yourDog.getName());
+
+
+
+            out.println(myDog.equals(yourDog));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
+        /*
+            singletone - дефолтный scope.
+            1) Такой бин создается сразу после прочтения Spring Container-ом конфиг файла
+            2) Является общим для всех, кто запросит его у Spring Container-а
+            3) Подходит для stateless объектов
+            4) Оба переменные ссылаются на один объека.
+            5) Singletone - может иметь только один объект у каждого класса
+        */
+
+
+        /*
+            Prototype
+            1) Такой бин создается только после обращения к Spring Container'у с помощью метода getBean
+            2) Для каждого такого обращения создается новый бин в Spring Container-е
+            3) Подходит для stateful объектов
+
+         */
 
 
 
         /*
-            singletone - дефолтный scope.
-            1) Такой бин создатеся сразу после прочтения Spring Container-ом конфиг файла
-            2) Является общим для всех, кто запросит его у Spring Container-а
-            3) Подходит для stateless объектов
+            "Stateful object" (объект с состоянием) - это термин, который используется в программировании
+            для описания объекта, который хранит свое состояние между вызовами методов.
+            В контексте объектно-ориентированных языков программирования,
+            таких как Java, состояние объекта обычно определяется значениями его полей (переменных экземпляра).
         */
 
         context.close();
